2d8799ea49c7ac393bb7c9a9e4a25da49caf0a3f
MateusSchulz <mateus.schulz.m@gmail.com>
2017-05-10 10:28:08 +0000

    Alterando Admin

--- Admin/Admin.rb
+++ Admin/Admin.rb
@@ -1,53 +1,29 @@
 require "./../Aluno-professor/Pessoa.rb"
 require "./../Documentos/trabalho.rb"
-require "./../Aluno-professor/ProgramaPrincipal.rb"
 
-class Admin < Pessoa
+class Admin
 
-	attr_reader :doc
 
-	def receber_documento ( doc )
+	def receber_documento
 		
-		@doc = doc
-		#@doc.hora_recebida = ...
+		professor = Professor.new("Annabell","123498", "81728172817","CCT", "Análise de Sólido")
+		aluno = Aluno.new("Schulz Correia", "40028922", "00000000000", "Ciência da Computação")
+		doc = Trabalho.new(aluno, "Análise do Solo de Campos dos Goytacaez", "Simulações Numéricas")
+
+#		selecionar_professor(professor01)
+		professor_avalia(professor, doc)
 
 	end
 
-	def selecionar_professor (professor)
+#	def selecionar_professor (professor)
+#
+#	end
 
-		# implementar sistema de seleção dos professores
+	def professor_avalia(professor, doc)
 
-		@doc.set_avaliador (professor)
-		#@doc.hora_enviada = ...
-
-
-	end
-
-	def professor_avalia
-
-		#@doc.set_nota = professor.get_nota (@doc)
+		doc.alterar_nota = professor.avaliar_documento(doc)
 
 	end
+
 
 end
-
-# sim, mas como enviamos para o aluno? eles vão ter um atributo documento tmb?
-# não tem como enviar para o aluno, o que seria isso?
-# eu pensei em um atributo da classe Aluno, onde ele teria o trabalho dele
-# eae nós podemos enviar somente a nota
-# isso, vamos ver na aula o que eles acham
-
-professor01 = Professor.new("Annabell",123498,123,1980,"Rua sei la","CCT")
-
-doc = Trabalho.new("Schulz Almeida Correia", "Análise do Solo de Campos dos Goytacaez", "Simulações Numéricas")
-
-test = Admin.new()
-test.receber_documento(doc)
-puts test.doc.info_trab
-puts test.selecionar_professor(professor01)
-
-
-# receber documento,
-# enviar documento - professor avaliar
-# verificar área (pra saber pra qual professor enviar o documento) - validação do seleciona_professor
-# registrar dados (hora do recebimento e do envio (professor) ) - como sabemos a hora que recebemos?
